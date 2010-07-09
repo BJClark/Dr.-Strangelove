b762f6b947d6f4c04c8742416573c670b62adf8f
BJ Clark <bjclark@me.com>
2010-07-08 23:21:26 -0700

    Some basic styling and layout

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,7 +1,7 @@
 module PhotosHelper
   def photo_navigation(photo)
     newer = Photo.find(:first, :conditions => ['id > ?', photo.id])
-    older = Photo.find(:first, :conditions => ['id < ?', photo.id])
+    older = Photo.find(:first, :conditions => ['id < ?', photo.id], :order => "id desc")
     out = ''
     out << (!newer.nil? ? link_to(image_tag("strangelove.jpg"), photo_url(newer), :class => "newer_photo") : empty_photo_nav_div("First Photo"))
     out << content_tag(:span, image_tag("strangelove.jpg"), :class => "current_photo")
@@ -10,6 +10,6 @@ module PhotosHelper
   end
   
   def empty_photo_nav_div(message)
-    content_tag(:div, message, :class => message.downcase.gsub(" ", "_"))
+    content_tag(:div, message, :class => [message.downcase.gsub(" ", "_"), "empty_photo"].join(" "))
   end
 end
