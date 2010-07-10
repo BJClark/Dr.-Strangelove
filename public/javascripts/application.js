3a40a59e0626f373c29fdb89f1731ffb328e3fd9
BJ Clark <bjclark@me.com>
2010-07-09 19:10:11 -0700

    Making JSLint a little happier.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,56 +6,56 @@ Photor.Tags = (function($){
 		var parent_div = $(target).closest('.tags');
 		$(parent_div).find('.add_tags').show();
 		$(target).hide();
-	}
+	};
 
 	var removeTag = function(tag){
 		var editable_tag = $(tag).closest('.editable_tag');
 		$(editable_tag).hide();
 		$.ajax({url: $(tag).attr('href'),
-					type: "POST",
-					data: {"_method": 'delete'},
-					success: function(data){
-						$(editable_tag).remove();
-						hideErrors(editable_tag);
-					},
-					error: function(data){
-						$(editable_tag).show();
-						handleError(data, editable_tag);
-					}
+			type: "POST",
+			data: {"_method": 'delete'},
+			success: function(data){
+				$(editable_tag).remove();
+				hideErrors(editable_tag);
+			},
+			error: function(data){
+				$(editable_tag).show();
+				handleError(data, editable_tag);
+			}
 		});
-	}
-	
+	};
+
 	var addTags = function(form) {
 		$.ajax({url: $(form).attr('action') + ".js",
-					type: "POST",
-					data: $(form).serialize(),
-					dataType: "html",
-					success: function(data){
-						var tags_div = $(form).closest('.tags');
-						$('.editable_tag', tags_div).remove();
-						$('.error_messages', tags_div).after(data);
-						hideErrors();
-						$(form).find('input[type=text]').val("");
-					},
-					error: function(data){
-						handleError(data, form);
-					}
+			type: "POST",
+			data: $(form).serialize(),
+			dataType: "html",
+			success: function(data){
+				var tags_div = $(form).closest('.tags');
+				$('.editable_tag', tags_div).remove();
+				$('.error_messages', tags_div).after(data);
+				hideErrors();
+				$(form).find('input[type=text]').val("");
+			},
+			error: function(data){
+				handleError(data, form);
+			}
 		});
-	}
-	
+	};
+
 	var hideErrors = function(wrapper) {
 		$(wrapper).parent(".tags").hide().text("");
-	}
+	};
 
 	var handleError = function(result, wrapper){
-		var message = ""
+		var message = "";
 		if(result.length > 0){
 			message = result;
 		} else {
 			message = "An unknown error occurred";
 		}
 		$(wrapper).parent(".tags").find(".error_messages").text(message).show();
-	}
+	};
 
 	// public
 	return {
@@ -69,19 +69,18 @@ Photor.Tags = (function($){
 				event.preventDefault();
 				removeTag(event.target);
 			});
-			
+
 			$('.add_tags').live('submit', function(event){
 				event.preventDefault();
 				addTags(event.target);
 			});
-
 		}
+		
 	}
-
 })(jQuery);
 
 $(document).ready(function(){
 	Photor.Tags.init();
 
 });
-	
+
